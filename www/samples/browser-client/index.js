79cd997e5cebb3658baf461d7538abefe21444de
Patrick Mueller <pmuellr@apache.org>
2014-12-04 21:38:50 -0800

    defining the JS API

--- www/samples/browser-client/index.js
+++ www/samples/browser-client/index.js
@@ -1,8 +1,56 @@
 // Licensed under the Apache License. See footer for details.
 
-toolcurb.sessionManager.getNewSessionKey(function(err, key) {
-  console.log("getNewSessionKey(" + err + ", " + key + ")")
-})
+var opts = {
+  url:      location.origin,
+  protocol: "sample",
+  key:      "0",
+}
+
+toolcurb.connectTarget(opts, cbTargetConnect)
+
+//------------------------------------------------------------------------------
+function cbTargetConnect(err, target) {
+  if (err) throw err
+
+  target.on("close", function(){})
+  target.on("error", function(error){})
+
+  target.on("clientsAttached", function(){})
+  target.on("clientsDetached", function(){})
+
+  target.on("request", function(command){
+    command.sendResponse(response)
+  })
+
+  target.sendEvent("connected")
+}
+
+//------------------------------------------------------------------------------
+
+var opts = {
+  url: location.origin,
+  key: "0"
+}
+
+toolcurb.connectClient(opts, cbClientConnect)
+
+//------------------------------------------------------------------------------
+function cbClientConnect(err, client) {
+  if (err) throw err
+
+  client.on("close", function(){})
+  client.on("error", function(error){})
+
+  client.on("targetConnected",    function(target){})
+  client.on("targetDisconnected", function(target){})
+
+  client.on("event", function(event){})
+
+  client.sendRequest(targetID, request, function(response){})
+  client.getTargets(function(targets){})
+  client.attachTarget(targetID)
+  client.detachTarget(targetID)
+}
 
 //------------------------------------------------------------------------------
 // Licensed under the Apache License, Version 2.0 (the "License");
